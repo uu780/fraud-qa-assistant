019f60287523b66ce294871fc7395717ef3f4e8d
uu780 <18846118645@163.com>
2026-05-06 08:39:35 +0800

    Update main.py

--- main.py
+++ main.py
@@ -158,4 +158,4 @@ async def cache_status():
 # 入口
 # ============================================================
 if __name__ == "__main__":
-    uvicorn.run(app, host="127.0.0.1", port=8089)
+    uvicorn.run(app, host="0.0.0.0", port=8089)
